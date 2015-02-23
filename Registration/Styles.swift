21aa42ba6462bd4bdfdb38e93d855de371725ba3
Shad <shad.j@outlook.com>
2015-02-23 01:28:16 +0000

    Added styles for button

--- Registration/Styles.swift
+++ Registration/Styles.swift
@@ -21,10 +21,25 @@ class CustomTextField: UITextField {
         
         // White text bro
         self.textColor = UIColor.whiteColor()
-        
-        
-    
-
     }
 }
 
+
+class CustomButton: UIButton {
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        // Corner radius
+        self.layer.cornerRadius = 5.0
+        
+        // Transparency, bro
+        self.backgroundColor = UIColor(red:184.0/255.0, green:184.0/255.0, blue:184.0/255.0, alpha:0.0)
+        
+        
+        // Border things
+        // UIColor cast to CGColor so no compilor error xox
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.whiteColor().CGColor
+        
+    }
+}
